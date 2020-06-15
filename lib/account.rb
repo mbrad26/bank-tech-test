dc79fdc084bf88c3c59fbb49b6751eb5d4afdbde
mbrad26 <mbrad26@gmail.com>
2020-06-15 16:58:22 +0100

    Account initializes with a balance of 0

--- lib/account.rb
+++ lib/account.rb
@@ -1,4 +1,11 @@
 class Account
+
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
   def deposit(amount)
   end
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,4 +3,8 @@ require 'account'
 describe Account do
   it { is_expected.to respond_to(:deposit).with(1).argument }
   it { is_expected.to respond_to(:withdraw).with(1).argument }
+
+  it 'has a default balance' do
+    expect(subject.balance).to eq 0
+  end
 end
