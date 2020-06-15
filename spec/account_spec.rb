2f05bab696b4599a92ca623973b00ca43bfb7538
mbrad26 <mbrad26@gmail.com>
2020-06-15 17:15:31 +0100

    #depopsit is adding amount to the account balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,7 +4,13 @@ describe Account do
   it { is_expected.to respond_to(:deposit).with(1).argument }
   it { is_expected.to respond_to(:withdraw).with(1).argument }
 
-  it 'has a default balance' do
+  it 'has a balance of 0 by default' do
     expect(subject.balance).to eq 0
+  end
+
+  describe '#deposit' do
+    it 'adds to the balance the amount provided as argument' do
+      expect { subject.deposit(1000) }.to change(subject, :balance).by 1000
+    end
   end
 end
