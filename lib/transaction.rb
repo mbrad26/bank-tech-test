cdb8aa20e82c737b2fdbe92e08b068b3d3c30a6b
mbrad26 <mbrad26@gmail.com>
2020-06-16 10:30:31 +0100

    Transaction instances instantiate with date, credit, debit and balance info

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,7 +1,12 @@
 class Transaction
   @@transactions = []
+  attr_reader :date, :credit, :debit, :balance
 
   def initialize(credit, debit, balance)
+    @date = Date.today.to_s
+    @credit = credit
+    @debit = debit
+    @balance = balance
   end
 
   def self.add_to_transactions(credit, debit, balance)
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,6 +3,16 @@ require 'transaction.rb'
 describe Transaction do
   subject { described_class }
 
+  it 'instantiates with date, credit, debit and balance' do
+    transaction = Transaction.new(10, '', 10)
+    allow(Date.today).to receive(:to_s) {"2020-06-16"}
+
+    expect(transaction.date).to eq "2020-06-16"
+    expect(transaction.credit).to eq 10
+    expect(transaction.debit).to eq ''
+    expect(transaction.balance).to eq 10
+  end
+
   describe '.add_to_transactions' do
     it 'records the transactions' do
       subject.add_to_transactions(100, '', 150)
@@ -10,4 +20,5 @@ describe Transaction do
       expect(subject.class_variable_get(:@@transactions)).to include instance_of subject
     end
   end
+
 end
